677214bbb05f2df7bcba7f2d6421922ae85b89ac
Andrew Hutchings <andrew.hutchings@gmail.com>
2010-09-23 02:16:19 -0400

    Add database config

---
+++ application/config/database.php
@@ -0,0 +1,31 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+return array
+(
+    'default' => array
+    (
+        'type'       => 'mysql',
+        'connection' => array(
+            /**
+             * The following options are available for MySQL:
+             *
+             * string   hostname     server hostname, or socket
+             * string   database     database name
+             * string   username     database username
+             * string   password     database password
+             * boolean  persistent   use persistent connections?
+             *
+             * Ports and sockets may be appended to the hostname.
+             */
+            'hostname'   => 'localhost',
+            'database'   => 'kohana-modules',
+            'username'   => 'kohana-modules',
+            'password'   => 'kohana-modules',
+            'persistent' => FALSE,
+        ),
+        'table_prefix' => '',
+        'charset'      => 'utf8',
+        'caching'      => FALSE,
+        'profiling'    => TRUE,
+    ),
+);
