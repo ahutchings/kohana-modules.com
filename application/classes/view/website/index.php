969d91118ca079fabe1c2422eceb3a09092f2762
Andrew Hutchings <andrew.hutchings@gmail.com>
2010-11-06 02:57:46 -0400

    Add module pagination

--- application/classes/view/website/index.php
+++ application/classes/view/website/index.php
@@ -3,16 +3,37 @@
 class View_Website_Index extends View_Layout
 {
     public $title = '';
+    
+    public $pagination;
 
     public function modules()
     {
         $modules = array();
 
-        foreach (ORM::factory('module')->find_all() as $module)
+        $count = DB::select(DB::expr('COUNT(*) AS count'))
+            ->from('modules')
+            ->execute()
+            ->get('count');
+
+        $this->pagination = Pagination::factory(array(
+            'total_items' => $count,
+            ));
+
+        $objects = ORM::factory('module')
+            ->limit($this->pagination->items_per_page)
+            ->offset($this->pagination->offset)
+            ->find_all();
+
+        foreach ($objects as $module)
         {
             $modules[] = $module->as_array();
         }
 
         return $modules;
+    }
+    
+    public function pagination()
+    {
+        return $this->pagination->render();
     }
 }
