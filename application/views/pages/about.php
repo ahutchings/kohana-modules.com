71ef394e5665ff2d7dd5538b9a3df7069e619ab5
Andrew Hutchings <andrew.hutchings@gmail.com>
2010-11-28 15:53:52 -0500

    Complete About page. Closes #13

--- application/views/pages/about.php
+++ application/views/pages/about.php
@@ -1,28 +1,30 @@
 <h2>About</h2>
 
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vulputate molestie dolor. In sollicitudin feugiat magna, sed ultricies magna congue vitae. Nulla turpis felis, tincidunt sed adipiscing nec, elementum non sapien. Nullam laoreet fermentum diam vitae vehicula. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean viverra arcu eu magna blandit egestas. Nunc rutrum scelerisque mollis. Aliquam sodales, neque nec congue porta, massa ligula vulputate elit, ut vehicula felis tortor at ipsum. Curabitur id libero ut orci varius sagittis. Nulla facilisi. Maecenas leo ligula, elementum vitae tristique ac, ultricies eget ante. In tristique rhoncus lectus ut iaculis. Suspendisse varius justo in tellus consequat faucibus. Duis vitae felis mollis felis fringilla pulvinar molestie ut odio.</p>
-
-<p>Nullam dapibus auctor facilisis. Etiam elementum consequat consequat. Phasellus leo ipsum, cursus at vestibulum nec, eleifend vel erat. In ac magna iaculis justo scelerisque volutpat a at lectus. Nulla tempus tortor non nunc placerat adipiscing. Cras viverra gravida pretium. Praesent nulla quam, tempor sed aliquam non, lacinia et augue. Aenean tristique ornare nunc feugiat dapibus. Vestibulum laoreet velit sed neque auctor id blandit elit dictum. Nam vitae porttitor risus. Phasellus eros enim, lacinia nec venenatis at, tempus et urna. Pellentesque hendrerit facilisis orci sit amet ultricies. Donec nec nibh justo, ut iaculis sapien. Morbi ipsum nisi, aliquam vitae convallis non, accumsan bibendum augue.</p>
+<p>kohana-modules.com was created to help <a href="http://kohanaframework.org">Kohana</a>
+developers find and discuss available modules. It is currently indexing Kohana
+v3.x modules on <a href="https://github.com">GitHub</a>.</p>
 
 <h3>Criteria for Inclusion</h3>
 
-<p>The repository must contain a single module at the top level. A user must be able to
-clone the repository into a Kohana application's modules directory, and have it work
-directly without moving files.</p>
+<p>The repository must contain a single module at the top level. A user must be
+able to clone the repository into a Kohana application's modules directory, and
+have it work directly without moving files.</p>
 
 <h3>Colophon</h3>
 
-<p>kohana-modules.com is running on <a href="http://kohanaframework.org">Kohana</a> v<?php echo Kohana::VERSION ?> with the following modules enabled:</p>
+<p>kohana-modules.com was built using <a href="http://kohanaframework.org">Kohana</a>
+v<?php echo Kohana::VERSION ?> and the <a href="http://www.blueprintcss.org">Blueprint CSS
+Framework</a>. The following Kohana modules are enabled:</p>
 
 <ul>
-    <li><?php echo HTML::anchor('https://github.com/kohana/auth', 'auth') ?></li>
-    <li><?php echo HTML::anchor('https://github.com/kohana/cache', 'cache') ?></li>
-    <li><?php echo HTML::anchor('https://github.com/ahutchings/kohana-cron-controller', 'cron-controller') ?></li>
-    <li><?php echo HTML::anchor('https://github.com/kohana/database', 'database') ?></li>
-    <li><?php echo HTML::anchor('https://github.com/ahutchings/kohana-github', 'github') ?></li>
-    <li><?php echo HTML::anchor('https://github.com/synapsestudios/kohana-notices', 'notices') ?></li>
-    <li><?php echo HTML::anchor('https://github.com/kohana/orm', 'orm') ?></li>
-    <li><?php echo HTML::anchor('https://github.com/kohana/pagination', 'pagination') ?></li>
-    <li><?php echo HTML::anchor('https://github.com/ThePixelDeveloper/kohana-sitemap', 'sitemap') ?></li>
-    <li><?php echo HTML::anchor('https://github.com/Zeelot/kohana-yform', 'yform') ?></li>
+    <li><?php echo HTML::anchor('https://github.com/kohana/auth', 'auth') ?> - Auth module for Kohana v3</li>
+    <li><?php echo HTML::anchor('https://github.com/kohana/cache', 'cache') ?> - Cache library for Kohana 3</li>
+    <li><?php echo HTML::anchor('https://github.com/ahutchings/kohana-cron-controller', 'cron-controller') ?> - Controller to handle cron and cli requests</li>
+    <li><?php echo HTML::anchor('https://github.com/kohana/database', 'database') ?> - A Kohana module for database interactions, building queries, and prepared statements </li>
+    <li><?php echo HTML::anchor('https://github.com/ahutchings/kohana-github', 'github') ?> - GitHub API module for Kohana 3</li>
+    <li><?php echo HTML::anchor('https://github.com/synapsestudios/kohana-notices', 'notices') ?> - Notices module for Kohana 3.x</li>
+    <li><?php echo HTML::anchor('https://github.com/kohana/orm', 'orm') ?> - Kohana ORM</li>
+    <li><?php echo HTML::anchor('https://github.com/kohana/pagination', 'pagination') ?> - A Kohana module for pagination</li>
+    <li><?php echo HTML::anchor('https://github.com/ThePixelDeveloper/kohana-sitemap', 'sitemap') ?> - A Kohana 3 Sitemap Class. Includes support for Google™ Mobile, Video, News, Code and Geo XML Sitemaps</li>
+    <li><?php echo HTML::anchor('https://github.com/Zeelot/kohana-yform', 'yform') ?> - Form Generation</li>
 </ul>
