45b470e37e2d75b6cfd1fedd6cb7a55151681e48
Andrew Hutchings <andrew.hutchings@gmail.com>
2010-11-30 18:56:04 -0500

    Add feed controller with recently_added method

---
+++ application/classes/controller/feed.php
@@ -0,0 +1,31 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Controller_Feed extends Controller
+{
+    public function action_index()
+    {
+        return $this->action_recently_added();
+    }
+    
+    public function action_recently_added()
+    {
+        $items = DB::select(
+                array(DB::expr('CONCAT(username,"/",name)'), 'title'),
+                array(DB::expr('CONCAT("modules/",username,"/",name)'), 'link'),
+                array('created_at', 'pubDate'),
+                'description'
+            )
+            ->from('modules')
+            ->order_by('created_at', 'DESC')
+            ->limit(25)
+            ->execute();
+            
+        $info = array(
+            'title'       => 'Kohana Modules',
+            'description' => 'Recently added modules at kohana-modules.com.',
+            'pubDate'     => $items[0]['pubDate'],
+            );
+        
+        echo Feed::create($info, $items);
+    }
+}
