2b5f32b5323fd1eeca5f002d4627c59eeb41e140
Andrew Hutchings <andrew.hutchings@gmail.com>
2010-11-29 22:39:16 -0500

    Use correct timezone

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -8,7 +8,7 @@
  * @see  http://kohanaframework.org/guide/using.configuration
  * @see  http://php.net/timezones
  */
-date_default_timezone_set('America/Chicago');
+date_default_timezone_set('America/New_York');
 
 /**
  * Set the default locale.
