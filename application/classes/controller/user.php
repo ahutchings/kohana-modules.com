b91f05d0af8bb8a2e9cf70dd40d7fcdba55aa830
Andrew Hutchings <andrew.hutchings@gmail.com>
2010-11-30 19:41:09 -0500

    Update login page title

--- application/classes/controller/user.php
+++ application/classes/controller/user.php
@@ -11,7 +11,7 @@ class Controller_User extends Controller_Template
     
     public function action_login()
     {
-        $this->template->title   = 'Login | ';
+        $this->template->title   = 'Login - ';
         $this->template->content = View::factory('user/login')
             ->bind('form', $form);
 
