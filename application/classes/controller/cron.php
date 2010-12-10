1958a4d34189c49cb525cbc6fe6d60a9bf0594c8
Andrew Hutchings <andrew.hutchings@gmail.com>
2010-12-09 19:50:26 -0500

    Finish moving all jobs to Job class

--- application/classes/controller/cron.php
+++ application/classes/controller/cron.php
@@ -23,21 +23,7 @@ class Controller_Cron extends Kohana_Controller_Cron
      */
     public function action_flag_deleted()
     {
-        foreach (ORM::factory('module')->find_all() as $module)
-        {
-            $url = 'https://github.com/'.$module->username.'/'.$module->name;
-            
-            if (Remote::status($url) === 404)
-            {
-                DB::update('modules')
-                    ->set(array('flagged_for_deletion_at' => time()))
-                    ->where('username', '=', $module->username)
-                    ->where('name', '=', $module->name); 
-            }
-            
-            // throttle HEAD requests
-            sleep(2);
-        }
+        Job::flag_deleted();
     }
     
     /**
@@ -45,20 +31,7 @@ class Controller_Cron extends Kohana_Controller_Cron
      */
     public function action_refresh_metadata()
     {
-        // select 30 jobs with oldest metadata
-        $modules = ORM::factory('module')
-            ->where('updated_at', '<', time() - Date::WEEK)
-            ->order_by('updated_at', 'ASC')
-            ->limit(30)
-            ->find_all();
-
-        foreach ($modules as $module)
-        {
-            $module->refresh_github_metadata();
-            
-            // throttle API requests
-            sleep(2);
-        }
+        Job::refresh_metadata();
     }
     
     /**
@@ -66,27 +39,7 @@ class Controller_Cron extends Kohana_Controller_Cron
      */
     public function action_import_from_search()
     {
-        for ($i = 1; $i < 10; $i++)
-        {
-            $results = Github::instance()
-                ->getRepoApi()
-                ->search('kohana', '', $i);
-
-            foreach ($results as $result)
-            {
-                $queue = ORM::factory('queue');
-                $queue->values($result);
-                $queue->source = Model_Queue::SOURCE_GITHUB_SEARCH;
-
-                if ($queue->check())
-                {
-                    $queue->save();   
-                }
-            }
-            
-            // throttle API requests
-            sleep(2);
-        }
+        Job::import_from_search();
     }
     
     /**
@@ -94,19 +47,6 @@ class Controller_Cron extends Kohana_Controller_Cron
      */
     public function action_prune_queue()
     {
-        $modules = ORM::factory('module')->find_all();
-        
-        foreach ($modules as $module)
-        {
-            $queue = ORM::factory('queue')
-                ->where('username', '=', $module->username)
-                ->where('name', '=', $module->name)
-                ->find();
-                
-            if ($queue->loaded())
-            {
-                $queue->delete();
-            }
-        }
+        Job::prune_queue();
     }
 }
--- application/classes/job.php
+++ application/classes/job.php
@@ -31,6 +31,9 @@ class Job
          }
     }
     
+    /**
+     * Imports new repositories from kolanos/kohana-universe.
+     */
     public static function import_from_universe()
     {
         $matches = self::fetch_gitmodules("https://github.com/kolanos/kohana-universe/raw/master/.gitmodules");
@@ -45,6 +48,98 @@ class Job
             if ($queue->check())
             {
                 $queue->save();
+            }
+        }
+    }
+    
+    /**
+     * Flags modules that have been removed from GitHub.
+     */
+    public static function flag_deleted()
+    {
+        foreach (ORM::factory('module')->find_all() as $module)
+        {
+            $url = 'https://github.com/'.$module->username.'/'.$module->name;
+            
+            if (Remote::status($url) === 404)
+            {
+                DB::update('modules')
+                    ->set(array('flagged_for_deletion_at' => time()))
+                    ->where('username', '=', $module->username)
+                    ->where('name', '=', $module->name); 
+            }
+            
+            // throttle HEAD requests
+            sleep(2);
+        }
+    }
+    
+    /**
+     * Refreshes local repository metadata from GitHub.
+     */
+    public static function refresh_metadata()
+    {
+        // select 30 jobs with oldest metadata
+        $modules = ORM::factory('module')
+            ->where('updated_at', '<', time() - Date::WEEK)
+            ->order_by('updated_at', 'ASC')
+            ->limit(30)
+            ->find_all();
+
+        foreach ($modules as $module)
+        {
+            $module->refresh_github_metadata();
+            
+            // throttle API requests
+            sleep(2);
+        }
+    }
+    
+    /**
+     * Fetches search results from GitHub and stores them locally.
+     */
+    public static function import_from_search()
+    {
+        for ($i = 1; $i < 10; $i++)
+        {
+            $results = Github::instance()
+                ->getRepoApi()
+                ->search('kohana', '', $i);
+
+            foreach ($results as $result)
+            {
+                $queue = ORM::factory('queue');
+                $queue->values($result);
+                $queue->source = Model_Queue::SOURCE_GITHUB_SEARCH;
+
+                if ($queue->check())
+                {
+                    $queue->save();   
+                }
+            }
+            
+            // throttle API requests
+            sleep(2);
+        }
+    }
+    
+    /**
+     * Delete search results that have been added to the module index.
+     */
+    public static function prune_queue()
+    {
+        $modules = ORM::factory('module')->find_all();
+        
+        foreach ($modules as $module)
+        {
+            $queue = ORM::factory('queue')
+                ->where('username', '=', $module->username)
+                ->where('name', '=', $module->name)
+                ->find();
+                
+            if ($queue->loaded())
+            {
+                $queue->delete();
             }
         }
     }
