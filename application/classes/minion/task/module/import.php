ef4ddb42e8c6a1e98767936ad834628b8a551d16
Andrew Hutchings <andrew.hutchings@gmail.com>
2011-01-07 05:33:15 -0500

    Import modules with Kohana compatibility relations

--- application/classes/minion/task/module/import.php
+++ application/classes/minion/task/module/import.php
@@ -4,12 +4,8 @@
  * Imports new repositories from the master module repository.
  */
 class Minion_Task_Module_Import extends Minion_Task
-{
-    /**
-	 * A set of config options that this task accepts
-	 * @var array
-	 */
-	protected $_config = array();
+{   
+    protected $_branches = array();
     
 	/**
 	 * Execute the task with the specified set of config
@@ -18,33 +14,103 @@ class Minion_Task_Module_Import extends Minion_Task
 	 */
 	public function execute(array $config)
 	{
-	    $matches = self::fetch_gitmodules("https://github.com/ahutchings/kohana-modules/raw/master/.gitmodules");
-
-         for ($i = 0; $i < count($matches[0]); $i++)
-         {
-             $count = ORM::factory('module')
-                 ->where('username', '=', $matches['username'][$i])
-                 ->where('name', '=', $matches['name'][$i])
-                 ->count_all();
-
-             if ($count === 0)
-             {
-                 $module = ORM::factory('module');
-                 $module->username = $matches['username'][$i];
-                 $module->name     = $matches['name'][$i];
-                 $module->save();
-
-                 $module->refresh_github_metadata();
-
-                 // throttle API requests
-                 sleep(2);
-             }
-         }
+	    $this->_fetch_branches();
+	    $this->_create_kohana_versions();
+	    
+	    foreach ($this->_branches as $branch)
+	    {
+	        $this->_import_branch($branch);
+	    }
          
-         self::prune_queue();
+        $this->_prune_queue();
 	}
 	
-	public static function fetch_gitmodules($url)
+	/**
+	 * Fetches branch names from the kohana-modules repository
+	 * and stores them in an instance variable.
+	 */
+	private function _fetch_branches()
+	{
+		$results = Github::instance()
+            ->getRepoApi()
+            ->getRepoBranches('ahutchings', 'kohana-modules');
+        
+        $this->_branches = array_keys($results);
+	}
+	
+	/**
+	 * Creates a kohana_version for each branch in the kohana-modules repo.
+	 */
+	private function _create_kohana_versions()
+	{
+	    foreach ($this->_branches as $branch)
+	    {
+            $count = DB::select('COUNT("*") AS mycount')
+                ->from('kohana_versions')
+                ->where('name', '=', $branch)
+                ->execute()
+                ->get('mycount');
+
+            if ($count == 0)
+            {
+                $query = DB::query(Database::INSERT, 'INSERT INTO kohana_versions (name) VALUES (:branch)')
+                    ->param(':branch', $branch)
+                    ->execute();
+            }
+	    }
+	}
+	
+	private function _import_branch($branch)
+	{
+	    $version_id = DB::select('id')
+	        ->from('kohana_versions')
+	        ->where('name', '=', $branch)
+	        ->execute()
+	        ->get('id');
+
+	    $matches = $this->_fetch_gitmodules("https://github.com/ahutchings/kohana-modules/raw/$branch/.gitmodules");
+
+        for ($i = 0; $i < count($matches[0]); $i++)
+        {
+            $module = ORM::factory('module')
+                ->where('username', '=', $matches['username'][$i])
+                ->where('name', '=', $matches['name'][$i])
+                ->find();
+
+            if ( ! $module->loaded())
+            {
+
+                $module->username = $matches['username'][$i];
+                $module->name     = $matches['name'][$i];
+                $module->save();
+
+                $module->refresh_github_metadata();
+
+                // throttle API requests
+                sleep(2);
+            }
+            
+            // Create a compatibility entry for the version
+            $count = DB::select('COUNT("*") AS mycount')
+                ->from('module_compatibilities')
+                ->where('module_id', '=', $module->id)
+                ->where('kohana_version_id', '=', $version_id)
+                ->execute()
+                ->get('mycount');
+                
+            if ($count == 0)
+            {
+                DB::query(Database::INSERT,
+                    "INSERT INTO module_compatibilities (module_id, kohana_version_id)
+                    VALUES (:module_id, :version_id)")
+                    ->param(':module_id', $module->id)
+                    ->param(':version_id', $version_id)
+                    ->execute();
+            }
+        }
+	}
+	
+	private function _fetch_gitmodules($url)
     {
         $pattern = "/git:\/\/github\.com\/(?P<username>.*)\/(?P<name>.*)\.git/i";
 
@@ -58,7 +124,7 @@ class Minion_Task_Module_Import extends Minion_Task
     /**
      * Delete search results that have been added to the module index.
      */
-    public static function prune_queue()
+    private function _prune_queue()
     {
         $modules = ORM::factory('module')->find_all();
         
