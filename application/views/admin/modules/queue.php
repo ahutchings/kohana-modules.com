1d20cc999fdb7434ccaa5d8832ad614e01ac9878
Andrew Hutchings <andrew.hutchings@gmail.com>
2011-01-18 20:39:01 -0500

    Remove user-submitted section from the admin module queue

--- application/views/admin/modules/queue.php
+++ application/views/admin/modules/queue.php
@@ -1,18 +1,3 @@
-<h3>User-Submitted</h3>
-
-<table class="span-24">
-    <thead>
-        <tr>
-            <th align="left">Username</th>
-            <th align="left">Name</th>
-            <th align="left">Description</th>
-            <th>Links</th>
-        </tr>
-    </thead>
-    <tbody>
-    </tbody>
-</table>
-
 <h3>Kohana Universe</h3>
 
 <table class="span-24">
