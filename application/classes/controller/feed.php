9c38d797f9f7ad4c2ea2eb448d098bf24184fe8b
Andrew Hutchings <andrew.hutchings@gmail.com>
2011-01-28 13:48:44 -0500

    Fix feed breakage

--- application/classes/controller/feed.php
+++ application/classes/controller/feed.php
@@ -14,7 +14,7 @@ class Controller_Feed extends Controller
     
     public function action_recently_added()
     {
-        $items = DB::select(
+        $results = DB::select(
                 array(DB::expr('CONCAT(username,"/",name)'), 'title'),
                 array(DB::expr('CONCAT("modules/",username,"/",name)'), 'link'),
                 array('created_at', 'pubDate'),
@@ -24,6 +24,14 @@ class Controller_Feed extends Controller
             ->order_by('created_at', 'DESC')
             ->limit(25)
             ->execute();
+            
+        $items = $results->as_array();
+
+        // Encode HTML special characters in the description.
+        for ($i = 0, $n = count($items); $i < $n; $i++)
+        {
+            $items[$i]['description'] = HTML::chars($items[$i]['description']);
+        }
             
         $info = array(
             'title'       => 'Kohana Modules',
