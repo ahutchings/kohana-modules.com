6e54b79e094caa29feaf97bd14c56f32f8887116
Andrew Hutchings <andrew.hutchings@gmail.com>
2011-06-01 16:59:02 -0400

    Quick fix to bypass GitHub cert verification

--- application/classes/git/repository.php
+++ application/classes/git/repository.php
@@ -43,7 +43,15 @@ class Git_Repository
         {
             $url = 'https://github.com/ahutchings/kohana-modules/raw/'.$branch.'/.gitmodules';
 
-            $gitmodules = Request::factory($url)
+            $request = Request::factory($url);
+
+            $client = $request->get_client()
+                ->options(array(
+                    CURLOPT_SSL_VERIFYPEER => FALSE,
+                    CURLOPT_SSL_VERIFYHOST => FALSE
+                    ));
+
+            $gitmodules = $request
                 ->execute()
                 ->body();
 
