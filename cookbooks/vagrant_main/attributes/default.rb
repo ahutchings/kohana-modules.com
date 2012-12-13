972444615dc36f00655a259dc03c269652b33c84
Andrew Hutchings <ahutchings@webmd.net>
2012-12-13 13:53:56 -0500

    Update docroot for Vagrant

--- cookbooks/vagrant_main/attributes/default.rb
+++ cookbooks/vagrant_main/attributes/default.rb
@@ -1,5 +1,5 @@
 default[:app][:server_name]        = "localhost"
 default[:app][:server_aliases]     = ["*.localhost"]
-default[:app][:docroot]            = "/home/vagrant/web-app"
+default[:app][:docroot]            = "/home/vagrant/web-app/public"
 default[:app][:kohana_environment] = "development"
 default[:app][:extra_packages]     = []
