ca2070ca6d0999e077caf96ecd5c773765f99279
Andrew Hutchings <andrew.hutchings@gmail.com>
2012-12-28 23:23:28 -0500

    Update reference to old Github module

--- application/classes/Controller/Admin/Dashboard.php
+++ application/classes/Controller/Admin/Dashboard.php
@@ -2,37 +2,38 @@
 
 class Controller_Admin_Dashboard extends Controller_Admin
 {
-    public function action_index()
-    {
-        $this->template->title = 'Dashboard - ';
-        $this->template->content = View::factory('admin/dashboard')
-            ->set('open_tickets', $this->_open_tickets())
-            ->bind('newest', $newest)
-            ->bind('recently_updated', $recently_updated);
+	public function action_index()
+	{
+		$this->template->title = 'Dashboard - ';
+		$this->template->content = View::factory('admin/dashboard')
+			->set('open_tickets', $this->_open_tickets())
+			->bind('newest', $newest)
+			->bind('recently_updated', $recently_updated);
 
-        $newest = ORM::factory('Module')
-            ->order_by('created_at', 'DESC')
-            ->limit(5)
-            ->find_all();
+		$newest = ORM::factory('Module')
+			->order_by('created_at', 'DESC')
+			->limit(5)
+			->find_all();
 
-        $recently_updated = ORM::factory('Module')
-            ->order_by('updated_at', 'DESC')
-            ->limit(5)
-            ->find_all();
-    }
+		$recently_updated = ORM::factory('Module')
+			->order_by('updated_at', 'DESC')
+			->limit(5)
+			->find_all();
+	}
 
-    private function _open_tickets()
-    {
-        $open_tickets = Kohana::cache('open_tickets');
+	private function _open_tickets()
+	{
+		$open_issues = Kohana::cache('open_issues');
 
-        if ($open_tickets !== NULL)
-        {
-            return $open_tickets;
-        }
+		if ($open_issues === NULL)
+		{
+			$github = new Github();
+			$repo = $github->get_repo('ahutchings', 'kohana-modules');
+			$open_issues = $repo->open_issues;
 
-        $repo = Github::instance()->getRepoApi()->show('ahutchings', 'kohana-modules');
-        Kohana::cache('open_tickets', $repo['open_issues']);
+			Kohana::cache('open_issues', $open_issues);
+		}
 
-        return $repo['open_issues'];
-    }
+		return $open_issues;
+	}
 }
