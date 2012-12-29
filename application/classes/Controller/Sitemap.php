cff5c14f5db6989656e7f17004005660562d1abb
Andrew Hutchings <andrew.hutchings@gmail.com>
2012-12-28 22:56:14 -0500

    PSR-0 updates

--- application/classes/Controller/Sitemap.php
+++ application/classes/Controller/Sitemap.php
@@ -22,7 +22,7 @@ class Controller_Sitemap extends Controller
 
         $url = new Sitemap_URL;
 
-        $url->set_loc(url::base('http'))
+        $url->set_loc(URL::base('http'))
             ->set_priority(1.0);
 
         $sitemap->add($url);
@@ -32,7 +32,7 @@ class Controller_Sitemap extends Controller
         {
             $url = new Sitemap_URL;
 
-            $url->set_loc(url::site("pages/$page", TRUE))
+            $url->set_loc(URL::site("pages/$page", TRUE))
                 ->set_priority(0.8);
 
             $sitemap->add($url);
@@ -44,7 +44,7 @@ class Controller_Sitemap extends Controller
         {
             $url = new Sitemap_URL;
 
-            $url->set_loc(url::site("modules/".$result['username'], TRUE))
+            $url->set_loc(URL::site("modules/".$result['username'], TRUE))
                 ->set_change_frequency('monthly')
                 ->set_priority(0.5);
 
@@ -56,7 +56,7 @@ class Controller_Sitemap extends Controller
         {
             $url = new Sitemap_URL;
 
-            $url->set_loc(url::site("modules/$module->username/$module->name", TRUE))
+            $url->set_loc(URL::site("modules/$module->username/$module->name", TRUE))
                 ->set_last_mod($module->updated_at)
                 ->set_change_frequency('monthly')
                 ->set_priority(0.5);
