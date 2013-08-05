ac5cf7d2e2f0c9d56f9525f00190d23c2f99a759
Andrew Hutchings <andrew.hutchings@gmail.com>
2013-08-05 19:48:26 -0400

    Fix indentation

--- application/views/admin/modules/pendingdeletion.php
+++ application/views/admin/modules/pendingdeletion.php
@@ -14,16 +14,16 @@
     <tbody>
     <?php foreach ($modules as $module): ?>
         <tr>
-        <td><?php echo $module->id ?></td>
-        <td>
-            <?php echo HTML::anchor($module->url('username'), $module->username) ?>
-        </td>
-        <td>
-            <?php echo HTML::anchor("modules/$module->username/$module->name", $module->name) ?>
-        </td>
-        <td><?php echo HTML::chars($module->description) ?></td>
-        <td><?php echo date(Date::$timestamp_format, $module->created_at) ?></td>
-        <td><?php echo date(Date::$timestamp_format, $module->updated_at) ?></td>
+            <td><?php echo $module->id ?></td>
+            <td>
+                <?php echo HTML::anchor($module->url('username'), $module->username) ?>
+            </td>
+            <td>
+                <?php echo HTML::anchor("modules/$module->username/$module->name", $module->name) ?>
+            </td>
+            <td><?php echo HTML::chars($module->description) ?></td>
+            <td><?php echo date(Date::$timestamp_format, $module->created_at) ?></td>
+            <td><?php echo date(Date::$timestamp_format, $module->updated_at) ?></td>
         </tr>
     <?php endforeach ?>
     </tbody>
