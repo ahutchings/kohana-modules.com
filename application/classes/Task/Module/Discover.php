d58edd98036461d6541f5a28e58350a710f65bb7
Andrew Hutchings <andrew.hutchings@gmail.com>
2013-08-29 23:36:43 -0400

    Remove kohana-universe import

--- application/classes/Task/Module/Discover.php
+++ application/classes/Task/Module/Discover.php
@@ -2,37 +2,36 @@
 
 class Task_Module_Discover extends Minion_Task
 {
-	/**
-	 * Execute the task with the specified set of config
-	 *
-	 * @return boolean TRUE if task executed successfully, else FALSE
-	 */
-	protected function _execute(array $params)
-	{
+    /**
+     * Execute the task with the specified set of config
+     *
+     * @return boolean TRUE if task executed successfully, else FALSE
+     */
+    protected function _execute(array $params)
+    {
         $this->_import_from_search();
-        $this->_import_from_universe();
-	}
+    }
 
-	/**
+    /**
      * Fetches search results from GitHub and stores them locally.
      */
     protected function _import_from_search()
     {
-		$i = 1;
+        $i = 1;
 
-		while (TRUE)
-		{
-			Minion_CLI::write("Searching page $i...");
+        while (TRUE)
+        {
+            Minion_CLI::write("Searching page $i...");
 
-			$results = Github::instance()
+            $results = Github::instance()
                 ->getRepoApi()
                 ->search('kohana', '', $i);
 
-			if (count($results) === 0)
-			{
-				Minion_CLI::write('Finished.');
-				break;
-			}
+            if (count($results) === 0)
+            {
+                Minion_CLI::write('Finished.');
+                break;
+            }
 
             foreach ($results as $result)
             {
@@ -50,42 +49,7 @@ class Task_Module_Discover extends Minion_Task
             // throttle API requests
             sleep(2);
 
-			$i++;
-		}
-    }
-
-    /**
-     * Imports new repositories from kolanos/kohana-universe.
-     */
-    protected function _import_from_universe()
-    {
-        $matches = self::fetch_gitmodules("https://raw.github.com/kolanos/kohana-universe/master/.gitmodules");
-
-        for ($i = 0; $i < count($matches[0]); $i++)
-        {
-            $queue = ORM::factory('Queue');
-            $queue->username = $matches['username'][$i];
-            $queue->name     = $matches['name'][$i];
-            $queue->source   = Model_Queue::SOURCE_KOHANA_UNIVERSE;
-
-            try
-            {
-               $queue->save();
-            }
-            catch (ORM_Validation_Exception $e) {}
+            $i++;
         }
-    }
-
-    public static function fetch_gitmodules($url)
-    {
-        $pattern = "/git:\/\/github\.com\/(?P<username>.*)\/(?P<name>.*)\.git/i";
-
-        $data = Request::factory($url)
-            ->execute()
-            ->body();
-
-        preg_match_all($pattern, $data, $matches);
-
-        return $matches;
     }
 }
